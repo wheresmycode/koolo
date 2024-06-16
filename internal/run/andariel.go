74410afc22f9c0d914b7c63caa9a7be30f0c35f1
wheresmycode <34865859+wheresmycode@users.noreply.github.com>
2024-06-16 12:40:43 +0200

    Added TP to andariel.go for companion mode
    Leader will now open a tp in CatacombsLevel4 if companion mode is enabled

--- internal/run/andariel.go
+++ internal/run/andariel.go
@@ -20,12 +20,26 @@ func (a Andariel) Name() string {
 	return string(config.AndarielRun)
 }
 
-func (a Andariel) BuildActions() []action.Action {
-	return []action.Action{
+func (a Andariel) BuildActions() (actions []action.Action) {
+	actions = append(actions,
 		a.builder.WayPoint(area.CatacombsLevel2), // Moving to starting point (Catacombs Level 2)
 		a.builder.MoveToArea(area.CatacombsLevel3),
 		a.builder.MoveToArea(area.CatacombsLevel4),
+	)
+	// Let's move to a safe area and open the portal in companion mode
+	if a.CharacterCfg.Companion.Enabled && a.CharacterCfg.Companion.Leader {
+		actions = append(actions,
+			a.builder.MoveToCoords(data.Position{
+				//22571, 9578
+				X: 22571,
+				Y: 9578,
+			}),
+			a.builder.OpenTPIfLeader(),
+		)
+	}
+	actions = append(actions,
 		a.builder.MoveToCoords(andarielStartingPosition), // Travel to boss position
 		a.char.KillAndariel(),                            // Kill Andariel
-	}
+	)
+	return actions
 }
